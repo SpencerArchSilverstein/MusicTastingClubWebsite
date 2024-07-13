13e9881217ce59640e00841012abcf62db228ba6
SpencerArchSilverstein <spencerarchsilverstein@gmail.com>
2024-07-13 10:17:51 -0400

    progress on FAQ page schedule

--- src/components/FAQ/CalendarSched.tsx
+++ src/components/FAQ/CalendarSched.tsx
@@ -6,6 +6,25 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { PickersDay, PickersDayProps } from '@mui/x-date-pickers/PickersDay';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { DayCalendarSkeleton } from '@mui/x-date-pickers/DayCalendarSkeleton';
+import { Grid, styled } from '@mui/material';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const Item2 = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(15.65),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 function getRandomNumber(min: number, max: number) {
   return Math.round(Math.random() * (max - min) + min);
@@ -93,24 +112,51 @@ export default function DateCalendarServerRequest() {
     setSelectedDate(date);
   };
 
+  const printed = (selectedDate: Dayjs) => {
+    if(selectedDate && selectedDate.format('YYYY-MM-DD') == '2024-09-16'){
+        return (<p>TEST</p>);
+    }
+  }
+ 
+  
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar
-        defaultValue={initialValue}
-        loading={isLoading}
-        onMonthChange={handleMonthChange}
-        onChange={handleDateChange}
-        renderLoading={() => <DayCalendarSkeleton />}
-        slots={{
-          day: ServerDay,
-        }}
-        slotProps={{
-          day: {
-            highlightedDays,
-          } as any,
-        }}
-      />
-      {selectedDate && <div>Selected Date: {selectedDate.format('YYYY-MM-DD')}</div>}
-    </LocalizationProvider>
+    <>
+    <Grid container spacing={2} columns={16}>
+
+        <Grid item xs={8}>
+            <Item>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateCalendar
+                        defaultValue={initialValue}
+                        loading={isLoading}
+                        onMonthChange={handleMonthChange}
+                        onChange={handleDateChange}
+                        renderLoading={() => <DayCalendarSkeleton />}
+                        slots={{
+                        day: ServerDay,
+                        }}
+                        slotProps={{
+                        day: {
+                            highlightedDays,
+                        } as any,
+                        }}
+                    />
+                {selectedDate && <div>Selected Date: {selectedDate!.format('YYYY-MM-DD')}</div>}
+                {<>{printed(selectedDate!)}</>}
+                </LocalizationProvider>
+            </Item>
+        </Grid>
+
+        <Grid item xs={8}>
+            <Item2>{selectedDate && <div>Selected Date: sdkahfjklajsdklfjaskj;df ajsdklfj aklds jdfklaskd faksl dfjklasjkdlfjaksldjfklasdk lfak dskj ljfks adk kfsd akfl klas kl{selectedDate!.format('YYYY-MM-DD')}</div>}
+             </Item2>
+                <br />
+                <Item><b>KEY:</b> Trivia | Genre History | Beef Watch | Music Edu</Item>
+                
+        </Grid>
+
+    </Grid>
+    </>
+   
   );
 }
